7a7f17a70a3b942d7c1421853150d581c0b2b874
ekdud01 <xuduorong12@gmail.com>
2026-08-31 15:43:36 +0900

    MyRunner Update

---
+++ src/main/java/com/rookies6/myspringboot4project/runner/MyRunner.java
@@ -0,0 +1,37 @@
+package com.rookies6.myspringboot4project.runner;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.function.Consumer;
+
+@Component
+public class MyRunner implements ApplicationRunner {
+    @Value("${spring.application.name}")
+    String applicationName;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println("MyRunner run() 호출됨!!");
+        System.out.println("Application Name = " + applicationName);
+
+        // Consumer 인터페이스를 Anonymous Inner Class로 표현.
+        args.getOptionNames().forEach(new Consumer<String>() {
+            @Override
+            public void accept(String s) {
+                System.out.println("arg name = " + s);
+            }
+        });
+
+        // args.getOptionNames() 메서드의 return 타입 Set<String>
+        // Iterable의 forEach(Consumer)
+        // Consumer의 추상 메서드 void accept(T t)
+        // Consumer 인터페이스를 람다식으로 표현.
+        args.getOptionNames().forEach(name -> System.out.println(name));
+
+        // Consumer 인터페이스를 Method Reference로 표현.
+        args.getOptionNames().forEach(System.out::println);
+    }
+}
