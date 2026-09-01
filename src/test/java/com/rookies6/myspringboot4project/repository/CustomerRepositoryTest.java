2e7b47cfd7cc1d83618d4841327f12165a7817db
ekdud01 <xuduorong12@gmail.com>
2026-09-01 17:06:12 +0900

    CustomerRepositoryTest

---
+++ src/test/java/com/rookies6/myspringboot4project/repository/CustomerRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.rookies6.myspringboot4project.repository;
+
+import com.rookies6.myspringboot4project.entity.Customer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class CustomerRepositoryTest {
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Test
+    void testCreate() {
+        // Given(준비 단계)
+        Customer customer = new Customer();
+        customer.setCustomerId("A004");
+        customer.setCustomerName("둘리");
+
+        // When(실행 단계)
+        Customer addCustomer = customerRepository.save(customer);
+
+        // Then(검증 단계)
+        assertThat(addCustomer).isNotNull();
+        assertThat(addCustomer.getCustomerName()).isEqualTo("둘리");
+
+    }
+}
