ccca8ca202b5566681958c55232f8853deb1c8ef
ekdud01 <xuduorong12@gmail.com>
2026-09-07 17:13:00 +0900

    update

--- src/main/java/com/rookies6/myspringboot4project/repository/StudentRepository.java
+++ src/main/java/com/rookies6/myspringboot4project/repository/StudentRepository.java
@@ -2,6 +2,8 @@ package com.rookies6.myspringboot4project.repository;
 
 import com.rookies6.myspringboot4project.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,7 +11,14 @@ import java.util.Optional;
 //StudentRepository 인터페이스
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    // 학번으로 조회 2개의 쿼리
     Optional<Student> findByStudentNumber(String studentNumber);
-    
+
     boolean existsByStudentNumber(String studentNumber);
+
+    // PK로 조회
+    //LEFT JOIN FETCH : 상세정보가 없는 학생도 조회되어야 하므로 외부 조인을 사용한다
+    @Query("SELECT s FROM Student s LEFT JOIN FETCH s.studentDetail WHERE s.id = :id")
+    Optional<Student> findByIdWithStudentDetail(@Param("id") Long id);
+
 }
